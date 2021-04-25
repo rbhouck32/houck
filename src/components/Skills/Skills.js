cb1da5403103ab9cc6ed7ed84108f7ffabb431f1
Rob <rbhouck32@gmail.com>
2021-04-25 15:18:33 -0400

    refactoring skills page so that label and icon align properly

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -62,32 +62,80 @@ const Skills = () => {
         <h1 ref={(e) => (headerScroll = e)}>My Skills</h1>
       </div>
       <div className="skills-container">
-        <div ref={(e) => (skillsScroll1 = e)} className="skill-wrap">
+        <div ref={(e) => (skillsScroll1 = e)} className="skill-wrap first">
           <FontAwesomeIcon icon={faHtml5} className="icon" size="6x" />
+          <div className="skill-label">
+            <p>HTML5</p>
+          </div>
         </div>
 
         <div ref={(e) => (skillsScroll2 = e)} className="skill-wrap">
           <FontAwesomeIcon icon={faJs} className="icon" size="6x" />
+          <div className="skill-label">
+            <p>JavaScript</p>
+          </div>
         </div>
         <div ref={(e) => (skillsScroll3 = e)} className="skill-wrap">
           <FontAwesomeIcon icon={faReact} className="icon" size="6x" />
+          <div className="skill-label react">
+            <p>React</p>
+          </div>
         </div>
         <div ref={(e) => (skillsScroll4 = e)} className="skill-wrap">
           <FontAwesomeIcon icon={faNode} className="icon" size="6x" />
+          <div className="skill-label node">
+            <p>NodeJs</p>
+          </div>
         </div>
         <div ref={(e) => (skillsScroll5 = e)} className="skill-wrap">
-          <FontAwesomeIcon icon={faLess} className="icon" size="6x" />
+          <div className="icon-wrap">
+            <FontAwesomeIcon icon={faLess} className="icon" size="6x" />
+          </div>
+          <div className="skill-label less">
+            <p>LESS</p>
+          </div>
         </div>
         <div ref={(e) => (skillsScroll6 = e)} className="skill-wrap">
-          <FontAwesomeIcon icon={faPython} className="icon" size="6x" />
+          <div className="icon-wrap python">
+            <FontAwesomeIcon icon={faPython} className="icon" size="6x" />
+          </div>
+
+          <div className="skill-label">
+            <p>Python</p>
+          </div>
         </div>
         <div ref={(e) => (skillsScroll7 = e)} className="skill-wrap">
           <FontAwesomeIcon icon={faDatabase} className="icon" size="6x" />
+          <div className="skill-label">
+            <p>RDBMS</p>
+          </div>
         </div>
         <div ref={(e) => (skillsScroll8 = e)} className="skill-wrap">
           <FontAwesomeIcon icon={faCss3Alt} className="icon" size="6x" />
+          <div className="skill-label">
+            <p>CSS3</p>
+          </div>
         </div>
       </div>
+      {/* <section className="lower-content">
+        <div className="skills-list">
+          <ul className="left-list">
+            <li>HTML5</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>NodeJS</li>
+          </ul>
+          <ul className="right-list">
+            <li>LESS</li>
+            <li>Python</li>
+            <li>RDBMS</li>
+            <li>CSS3</li>
+          </ul>
+        </div>
+        <div className="right-lower">
+          <h2></h2>
+        </div>
+      </section> */}
     </div>
   );
 };
