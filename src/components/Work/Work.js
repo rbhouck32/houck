93c2925657ab4385ef629279761f9c0b5795d6a8
Rob <rbhouck32@gmail.com>
2021-05-03 03:57:17 -0400

    added headerScroll animation to work page

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import WorkCSS from "../Work/Work.module.css";
+import { TweenMax, Power3 } from "gsap";
 
 const Work = () => {
+  let headerScroll = useRef(null);
+
+  useEffect(() => {
+    TweenMax.staggerFrom(
+      [headerScroll],
+      4,
+      {
+        opacity: 0,
+        x: 40,
+        ease: Power3.easeOut,
+      },
+      0.25
+    );
+  }, []);
   return (
     <div className={WorkCSS.container}>
-      <h1>My Work</h1>
+      <h1 ref={(e) => (headerScroll = e)}>My Work</h1>
     </div>
   );
 };
