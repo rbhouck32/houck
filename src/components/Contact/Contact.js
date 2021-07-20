3cee7216ce7e30f9d5dea50ed24b0c14d4f207f9
Rob <rbhouck32@gmail.com>
2021-07-20 16:08:49 -0400

    fixed email error

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -46,7 +46,7 @@ const Contact = () => {
   const onSubmit = async (data) => {
     console.log("submission starting", data);
     generateContactNumber();
-    sendForm("contact_form", "template_86v5uzu", "#contact-form")
+    sendForm("default_service", "template_86v5uzu", "#contact-form")
       .then(
         function (response) {
           console.log("SUCCESS!", response.status, response.text);
